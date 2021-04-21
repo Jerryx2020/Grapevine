f30b76b0de393ba0582b1b3704ef73dc307412c6
Jerryx2020 <80130323+Jerryx2020@users.noreply.github.com>
2021-04-21 18:04:17 -0500

    Added AlertView via upload

---
+++ Grapevine/Model/AlertView.swift
@@ -0,0 +1,33 @@
+//
+//  AlertView.swift
+//  Grapevine
+//
+//  Created by Jerry Xia on 3/16/21.
+//
+
+import SwiftUI
+
+func alertView(msg: String,completion: @escaping (String) -> ()){
+    
+    let alert = UIAlertController(title: "Message", message: msg, preferredStyle: .alert)
+    
+    alert.addTextField { (txt) in
+        txt.placeholder = msg.contains("Verification") ? "123456" : ""
+    }
+    
+    alert.addAction(UIAlertAction(title: "Cancel", style: .destructive))
+    
+    alert.addAction(UIAlertAction(title: msg.contains("Verification") ? "Verify" : "Update", style: .default, handler: { (_) in
+        
+        let code = alert.textFields![0].text ?? ""
+        
+        if code == ""{
+            // Repromoting..
+            UIApplication.shared.windows.first?.rootViewController?.present(alert, animated: true)
+            return
+        }
+        completion(code)
+    }))
+    
+    UIApplication.shared.windows.first?.rootViewController?.present(alert, animated: true)
+}
