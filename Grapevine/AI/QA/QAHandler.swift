f0d1b31f2ca8fda2099c8d2da52d0042d82e73cd
jhawgs <70661513+jhawgs@users.noreply.github.com>
2021-05-31 14:50:05 -0500

    added QA handler

---
+++ Grapevine/AI/QA/QAHandler.swift
@@ -0,0 +1,44 @@
+//
+//  QAHandler.swift
+//  Grapevine
+//
+//  Created by Nathan Bronson on 5/31/21.
+//
+
+import Foundation
+
+struct QAHandler {
+    let BertInterface: BertQAHandler = BertQAHandler()
+    let posts: [String]
+    
+    init(posts: [String] = []) {
+        self.posts = posts
+    }
+    
+    func ask(_ query: String, _ content: String) -> String { //query is question content is source
+        return self.BertInterface.run(query: query, content: content)?.answer.text.value ?? ""
+    }
+    
+    func find(_ query: String, _ sources: [String] = self.posts) -> String {
+        var answers: [String] = []
+        for i in sources {
+            let n: String = ask(query, i)
+            if !n.isEmpty {
+                answers.append(n)
+            }
+        }
+        if answers.count == 0 {
+            return "Sorry, we couldn't find anything."
+        } else if answers.count == 1 {
+            return answers[0]
+        } else {
+            var out: String = ""
+            out += answers[0]
+            answers.remove(at: 0)
+            for i in answers {
+                out += "\n"
+            }
+            return out
+        }
+    }
+}
