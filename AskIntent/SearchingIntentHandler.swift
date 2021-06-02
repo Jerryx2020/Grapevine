d8bcdf078a6120122dcbed0294138083576662ff
jhawgs <70661513+jhawgs@users.noreply.github.com>
2021-06-02 17:28:43 -0500

    began Siri

---
+++ AskIntent/SearchingIntentHandler.swift
@@ -0,0 +1,18 @@
+//
+//  SearchingIntentHandler.swift
+//  AskIntent
+//
+//  Created by Nathan Bronson on 6/2/21.
+//
+
+import Foundation
+
+class SearchingIntentHandler: NSObject, SearchingIntentHandling {
+    func confirm(intent: SearchingIntent, completion: @escaping (SearchingIntentResponse) -> Void) {
+        
+    }
+    
+    func handle(intent: SearchingIntent, completion: @escaping (SearchingIntentResponse) -> Void) {
+        
+    }
+}
