ae132a337b82a4ac71d8df8e08b5129bcad08978
jhawgs <70661513+jhawgs@users.noreply.github.com>
2021-06-13 15:31:41 -0500

    added user posts to user view

--- Grapevine/View/UserView.swift
+++ Grapevine/View/UserView.swift
@@ -11,6 +11,8 @@ import SDWebImageSwiftUI
 struct UserView: View {
     @Binding var selectedUser: UserModel?
     
+    @StateObject var postData = PostViewModel()
+    
     let User: UserModel
     var edges = UIApplication.shared.windows.first?.safeAreaInsets
     init(_ user: UserModel, _ sel: Binding<UserModel?>) {
@@ -29,7 +31,7 @@ struct UserView: View {
                         .background(Color.white.opacity(0.06))
                         .cornerRadius(15)
                 })
-                .padding()
+                .padding(.top)
                 .padding(.top)
                 .padding(.top)
                 Spacer(minLength: 0)
@@ -79,9 +81,22 @@ struct UserView: View {
             Text("_______________________________________")
                 .font(Font.custom("ITC Avant Garde Gothic Bold", size: 18))
                 .foregroundColor(.white)
+                .padding()
                 .padding(.top)
                 .padding(.top)
-                .padding(.top)
+            
+            ScrollView{
+                
+                VStack(spacing: 15){
+                    
+                    ForEach(self.postData.posts.filter({ $0.user.username == self.User.username })) {post in
+                        
+                        PostRow(selectedUser: self.$selectedUser, post: post, postData: postData)
+                    }
+                }
+                .padding()
+                .padding(.bottom,55)
+            }
             Spacer(minLength: 0)
         }
         .background(Color("bg"))
